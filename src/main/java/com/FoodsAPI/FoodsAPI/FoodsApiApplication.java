8e58ef5679b624a3bac856aab36537e1934207d3
sjesus <s.jesus10@yahoo.com>
2023-11-30 23:26:32 -0600

    first commit

---
+++ src/main/java/com/FoodsAPI/FoodsAPI/FoodsApiApplication.java
@@ -0,0 +1,13 @@
+package com.FoodsAPI.FoodsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FoodsAPI/FoodsAPI/FoodsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FoodsAPI.FoodsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
